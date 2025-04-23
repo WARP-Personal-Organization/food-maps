6f3ac774c38920cb1d061bae4c15ceb74e339785
CoolWhip <kertsolas@gmail.com>
2025-04-23 12:01:35 +0800

    chore: changed filter dish check to black

--- src/components/food-map/FilterDishesView.tsx
+++ src/components/food-map/FilterDishesView.tsx
@@ -94,7 +94,7 @@ const FilterDishesView: React.FC<FilterDishesViewProps> = ({
                   viewBox="0 0 24 24"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
-                  className="text-white"
+                  className="text-black"
                 >
                   <path
                     d="M9 16.17L4.83 12L3.41 13.41L9 19L21 7L19.59 5.59L9 16.17Z"
